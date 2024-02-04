498303b00f4f504ad36ecd259b3db93b31800534
unknown <lintg@sina.com>
2024-02-04 16:10:36 +0800

    feat: add code

--- packages/components/Modal/index.ts
+++ packages/components/Modal/index.ts
@@ -3,6 +3,6 @@ import './src/index.less'
 import basicModal from './src/BasicModal.vue'
 
 export const BasicModal = withInstall(basicModal)
-export { useModalContext } from '@h/useModalContext'
-export { useModal, useModalInner } from '@h/useModal'
+export { useModalContext } from '@hooks/useModalContext'
+export { useModal, useModalInner } from '@hooks/useModal'
 export * from './src/typing'
