6d7aa528b39e794504475dc863f74d0b8f56d250
unknown <lintg@sina.com>
2024-02-20 18:03:46 +0800

    feat: add code

--- packages/views/form-design/components/VFormDesign/config/custom/index.ts
+++ packages/views/form-design/components/VFormDesign/config/custom/index.ts
@@ -4,6 +4,5 @@ for (const path in modules) {
   let name = path.split('/').pop();
   name = name.split('.')[0];
   config[name] = modules[path].default;
-  // deepAssign(lang, modules[path].default)
 }
 export default config;
