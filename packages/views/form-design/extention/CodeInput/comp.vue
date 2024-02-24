3e89092af6d1c8f44ace8bd409f60cc5dd722bdd
unknown <lintg@sina.com>
2024-02-25 01:00:38 +0800

    feat: add code

--- packages/views/form-design/extention/CodeInput/comp.vue
+++ packages/views/form-design/extention/CodeInput/comp.vue
@@ -1,12 +1,7 @@
 <template>
-  <a-button @click="openModal" v-bind="$attrs"> {{ props.buttonText }} </a-button>
-  <BasicModal
-    destroyOnClose
-    @register="register"
-    :title="props.editorTitle"
-    @open-change="handleShow"
-    @ok="handleOk"
-  >
+  <a-button @click="handleShow" v-bind="$attrs"> {{ props.buttonText }} </a-button>
+  <BasicModal destroyOnClose @register="register" :title="props.editorTitle" @ok="handleOk">
+    <BasicForm @register="registerForm" />
     <CodeEditor
       v-model:value="state"
       :mode="props.mode"
@@ -21,6 +16,10 @@
   import { BasicModal, useModal } from '@c/Modal';
   import { useRuleFormItem } from '@h/component/useFormItem';
   import { propTypes } from '@utils/propTypes';
+  import { BasicForm, useForm } from '@c/Form';
+  // import VFormCreate from '../../components/VFormCreate/index.vue';
+  import { formConfig } from '../helper';
+  import { cloneDeep } from 'lodash-es';
 
   const props = defineProps({
     value: propTypes.string || propTypes.function,
@@ -32,9 +31,36 @@
     bordered: { type: Boolean, default: true },
   });
   const emit = defineEmits(['update:value']);
-  const loading = ref(true);
-  const lines = ref(10);
-
+  // const loading = ref(true);
+  // const lines = ref(10);
+  const attrs = ref(cloneDeep(formConfig));
+  const schemas = [
+    {
+      field: 'code_input',
+      component: 'Code',
+      label: '代码',
+      componentProps: {
+        mode: 'application/json',
+        bordered: true,
+        autoFormat: true,
+        readonly: false,
+        height: 10,
+        prefix: 'function(){',
+        suffix: '}',
+      },
+      colProps: {
+        span: 24,
+      },
+    },
+  ];
+  const [registerForm, { getFieldsValue, setFieldsValue }] = useForm({
+    schemas,
+    showActionButtonGroup: false,
+  });
+  // const fApi = ref({}),
+  //   formModel = ref({});
+  // const onSubmit = () => {};
+  //attrs.value.
   const [register, { openModal, closeModal }] = useModal();
   const [state] = useRuleFormItem(props, 'value', 'change');
   //const { t } = useI18n();
@@ -46,8 +72,15 @@
   );
   // Embedded in the form, just use the hook binding to perform form verification
 
-  const handleOk = () => {
+  const handleOk = async () => {
+    const data = await getFieldsValue();
+    // debugger;
+    state.value = data.code_input;
     closeModal();
   };
-  const handleShow = () => {};
+  const handleShow = async () => {
+    openModal();
+    const code_input = state.value;
+    await setFieldsValue({ code_input });
+  };
 </script>
---
+++ packages/views/form-design/extention/helper.ts
@@ -0,0 +1,10 @@
+export const formConfig = {
+  // 表单配置
+  schemas: [],
+  layout: 'horizontal',
+  labelLayout: 'flex',
+  labelWidth: 100,
+  labelCol: {},
+  wrapperCol: {},
+  showActionButtonGroup: false,
+};
