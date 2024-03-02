b453b5cb85dec0673e4188fb67cbbdc8ce068bee
unknown <lintg@sina.com>
2024-03-03 00:27:14 +0800

    feat: add code

---
+++ packages/views/form-design/extention/ApiRadioGroup/schema.ts
@@ -0,0 +1,23 @@
+import { optionsListApi } from '@/api/demo/select';
+
+export default {
+  component: 'ApiRadioGroup',
+  label: '远程Radio',
+  //  helpMessage: ['ApiRadioGroup组件', '使用接口提供的数据生成选项'],
+  required: true,
+  componentProps: {
+    api: optionsListApi,
+    params: {
+      count: 2,
+    },
+    resultField: 'list',
+    // use name as label
+    labelField: 'name',
+    // use id as value
+    valueField: 'id',
+  },
+  defaultValue: '1',
+  colProps: {
+    span: 8,
+  },
+};
