849d347e9376f7109200225527a8a5aa2ce4c598
unknown <lintg@sina.com>
2024-03-05 09:53:06 +0800

    feat: add code

--- packages/views/form-design/extention/Alert/comp.vue
+++ packages/views/form-design/extention/Alert/comp.vue
@@ -1,23 +1,8 @@
 <template>
-  <alert v-bind="props" />
+  <alert v-bind="attrs" />
 </template>
 <script setup>
-  import { useRuleFormItem } from '@h/component/useFormItem';
-  import { propTypes } from '@utils/propTypes';
   import { Alert } from 'ant-design-vue';
 
-  // const [state] = useRuleFormItem(props, 'value', 'change');
-  const props = defineProps({
-    value: propTypes.string || propTypes.function,
-    message: propTypes.string,
-    type: propTypes.string,
-    closable: propTypes.boolean,
-  });
-
-  // Embedded in the form, just use the hook binding to perform form verification
+  const attrs = useAttrs();
 </script>
-<style>
-  .code-prefix {
-    margin-left: 30px;
-  }
-</style>
