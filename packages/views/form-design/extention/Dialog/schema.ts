dcc931919763da0d1f3e1d1c9cd4d7019f0fe0ef
unknown <lintg@sina.com>
2024-03-26 00:10:10 +0800

    feat: add code

---
+++ packages/views/form-design/extention/Dialog/schema.ts
@@ -0,0 +1,14 @@
+export default {
+  label: '对话框',
+  formItem: false,
+  type: 'container',
+  icon: 'ant-design:border-outlined',
+  componentProps: {
+    title: '卡片',
+    bordered: false,
+    slots: {
+      extra: 'hi',
+    },
+  },
+  children: [],
+};
