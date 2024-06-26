b81b6e8ca2c8dad737dfb122dcaa748b97d7ff06
unknown <lintg@sina.com>
2024-06-26 23:34:30 +0800

    feat: add code

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,12 +1,14 @@
 import type { App } from 'vue';
 import { createPinia } from 'pinia';
 import { registerPiniaPersistPlugin } from '@store/plugin/persist';
+import app from '../App.vue';
 
 const store = createPinia();
 registerPiniaPersistPlugin(store);
+createApp(app).use(store);
 
 export function setupStore(app: App<Element>) {
-  app.use(store);
+  //app.use(store);
 }
 
 export { store };
