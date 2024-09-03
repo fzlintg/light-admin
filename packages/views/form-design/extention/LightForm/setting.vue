2c0649ed6bddc2be71e88993c5979cd71a4848e9
unknown <lintg@sina.com>
2024-09-04 00:18:58 +0800

    feat: add code

--- packages/views/form-design/extention/LightForm/setting.vue
+++ packages/views/form-design/extention/LightForm/setting.vue
@@ -22,13 +22,13 @@
 
   const [formState] = useRuleFormItem(props, 'props', 'update:props');
 
-  const state = ref({ logic: formState.value.componentProps.logic });
+  const state = ref({ name: formState.value.componentProps.logic });
   const updateFormModel = (value) => {
     Object.assign(state.value, value);
   };
   const loadForm = () => {
     console.log(formState.value);
-    formState.value.componentProps.logic = state.value.logic;
+    formState.value.componentProps.logic = state.value.name;
 
     //Object.assign(formState.value.componentProps, state.value);
   };
